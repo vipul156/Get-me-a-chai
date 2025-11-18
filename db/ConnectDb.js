5997c853473658f7adfd135ba2d75bfa80236a48
vipul156 <ak4780321@gmail.com>
2025-11-18 14:13:13 +0530

    fixed

--- db/ConnectDb.js
+++ db/ConnectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(`mongodb://localhost:27017/chai`, {
+    const connect = await mongoose.connect(`process.env.MONGO_URI`, {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected: {conn.connection.host}`);
